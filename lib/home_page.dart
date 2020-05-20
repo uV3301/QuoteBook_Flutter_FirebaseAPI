2799265a9cab0d27834c4b28b949786187a3e2c0
Yuvraj Singh <uv.gasni0303@gmail.com>
2020-05-21 01:00:13 +0530

    finishing commit

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,7 @@
 import 'dart:convert';
+import 'dart:math';
 import 'package:flutter/material.dart';
+import 'package:share/share.dart';
 import 'package:velocity_x/velocity_x.dart';
 import 'package:http/http.dart' as http;
 
@@ -9,9 +11,13 @@ class HomePage extends StatelessWidget {
     var res = await http.get(url);
     return jsonDecode(res.body)["headers"];
   }
-  final color = Colors.teal;
+  
+  
   @override
   Widget build(BuildContext context) {
+    final colors = [
+    Vx.green600, Vx.teal600, Vx.blue600, Vx.red500, Vx.yellow600, Vx.pink600, Vx.indigo600
+    ];
     return Material(
       child: FutureBuilder(
         future: fetchData(),
@@ -20,23 +26,30 @@ class HomePage extends StatelessWidget {
             if (snapshot.hasData) {
               return StatefulBuilder(
                 builder:(context, setState){
+                  final color = colors[Random().nextInt(7)];
                                return VxSwiper(  
                   scrollDirection: Axis.vertical,
                   height: context.screenHeight,
                   viewportFraction: 1.0,
+                  onPageChanged: (index) {
+                    setState((){});
+                  },
                   items: snapshot.data.map<Widget>(
                     (el)=> VStack(
                       [
-                        "Quotes".text.xl2.white.makeCentered(),
-                        "${el["quoteText"]}".text.xl2.italic.white.make().box.shadowMd.make(),
-                        IconButton(iconSize: 30,icon: Icon(Icons.share, color: Colors.white,),onPressed: null,),
+                        "Quotes".text.xl3.white.bold.makeCentered(),
+                        "${el["quoteText"]}".text.xl2.italic.white.make().box.shadowXl.make().p20(),
+                        "- ${el["quoteAuthor"]}".text.xl.white.make(),
+                        IconButton(focusColor: color,iconSize: 30,icon: Icon(Icons.share, color: Colors.white,),onPressed: () {
+                          Share.share("${el["quoteText"]}");
+                        },),
                         
                       ],
                       crossAlignment: CrossAxisAlignment.center,
-                      alignment: MainAxisAlignment.spaceAround,
+                      alignment: MainAxisAlignment.spaceEvenly,
                       )
                     .animatedBox
-                    .p16
+                    .p20
                     .color(color)
                     .make()
                     .h(context.screenHeight)
